7cc7f9c0466d5d1730ebfa4ca8023d11563494d7
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2018-09-24 11:57:11 +0200

    Initial commit

---
+++ server.js
@@ -0,0 +1,37 @@
+const Hapi = require('hapi');
+const Inert = require('inert');
+const Path = require('path');
+const Pug = require('pug');
+const Vision = require('vision');
+const Settings = require('./settings');
+const Routes = require('./lib/routes');
+const Models = require('./lib/models/');
+
+const server = new Hapi.Server({ port: Settings.port });
+
+const init = async () => {
+  await server.register([Vision, Inert]);
+
+  server.views({
+    engines: { pug: Pug },
+    path: Path.join(__dirname, 'lib/views'),
+    compileOptions: {
+      pretty: false,
+    },
+    isCached: Settings.env === 'production'
+  });
+
+  server.route(Routes);
+
+  await server.start();
+  console.log(`Server running at: ${server.info.uri}`);
+};
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  process.exit(1);
+});
+
+Models.sequelize.sync().then(() => {
+  init();
+});
