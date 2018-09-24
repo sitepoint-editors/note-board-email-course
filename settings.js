f37506905d300f0c06db15701be1b18bbfffbf62
James Hibbard <1940994+jameshibbard@users.noreply.github.com>
2018-09-24 13:43:30 +0200

    Made .env optional

--- settings.js
+++ settings.js
@@ -1,7 +1,5 @@
-// This will load our .env file and add the values to process.env,
-// IMPORTANT: Omit this line if you don't want to use this functionality
-
-require('dotenv').config({ silent: true });
+// Uncomment the following line to load our .env file and add the values to process.env
+// require('dotenv').config({ silent: true });
 
 module.exports = {
   port: process.env.PORT || 3000,
